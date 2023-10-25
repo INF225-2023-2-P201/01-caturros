cf000d1295c7f8b0f7fb8910588a2236c1ca4cde
pantrucas <vicente.ruizv@usm.cl>
2023-10-24 21:46:42 -0300

    a

--- descripcion.py
+++ descripcion.py
@@ -3,8 +3,9 @@ import spacy
 
 nlp = spacy.load("es_core_news_sm")
 
-text = ("A habilitar B")
+text = ("queiero A habilitar B")
 
+#Revisa verbos de la frase
 doc = nlp(text)
 verbo = [token.lemma_ for token in doc if token.pos_ == "VERB"][0]
 
