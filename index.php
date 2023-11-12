e4b3cb2ad7d9f0aee5acbe0ddf3184c13a948fb5
dmoyan0 <diego.moyano@usm.cl>
2023-11-12 19:01:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,47 +4,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <title>Caturros</title>
+    <link rel="stylesheet" href="style.css">
 
     <style>
-
-    form {
-      text-align: center;
-    }
-
-    label {
-      display: block;
-      margin-bottom: 30px;
-    }
-
-    input[type="text"] {
-      width: 800px; /* Puedes ajustar este valor según tus necesidades */
-      padding: 20px;
-      font-size: 16px;
-    }
-
-    input[type="submit"] {
-      padding: 10px;
-      font-size: 16px;
-    }
+        .menu{
+          display: flex;
+          margin-right: 1300px;
+        }
+        .container{
+          text-align: center;
+          justify-content: center;
+          align-items: center;
+        }
+        .entrada-label {
+          font-weight: bold;
+          margin-top: 10px;
+        }
+        .entrada-input {
+          width: 250px;
+          margin-left: 10px;
+          margin-top: 50px;
+        }
+        .salida-label {
+          font-weight: bold;
+          margin-top: 10px;
+        }
+        .salida-output {
+          color: black;
+          margin-top: 100px;
+        }
+        .calcular-button {
+          background-color: #006400;
+          color: white;
+          border: none;
+          padding: 20px 150px;
+          cursor: pointer;
+          margin-top: 10px;
+        }
     </style>
 </head>
 <body>
-    <header class="header">
+  <header class="header">
         <a href="#" class="logo"></a>
         <div class="menu container">
             <input type="checkbox" id="menu"/>
             <label for="menu">
-                <img src="./img/Santiago_Wanderers1.png" class="menu-ico" alt="menu">
+                <img src="images/Santiago_Wanderers1.png" class="menu-ico" alt="menu">
             </label>
         </div>
-    </header>
+  </header>
 
-    <form>
-        <label for="nombre">Intención:</label>
-        <input type="text" id="nombre" name="nombre">
-        <input type="submit" value="Enviar">
-    </form>
+
+  <div class="container">
+      <label for="entrada" class="entrada-label">Entrada:</label>
+      <input type="text" id="entrada" name="entrada" class="entrada-input" placeholder="Introduce un valor">
+
+      <br>
+
+      <label for="salida" class="salida-label">Salida:</label>
+      <output id="salida" name="salida" class="salida-output"></output>
+      
+      <br>
+
+      <button onclick="calcularResultado()" class="calcular-button">Calcular</button>
+  </div>
+      <script>
+          function calcularResultado() {
+              var valorEntrada = document.getElementById("entrada").value;
+              document.getElementById("salida").textContent = valorEntrada;
+          }
+      </script>
 </body>
 </html>
